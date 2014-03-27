dfae31f406ea7717f3beb25a2cc628954478ba41
Rob Styles <rob.styles@dynamicorange.com>
2014-03-27 12:29:22 +0000

    Calcuating scores and returning sets

--- lib/commendo/content_set.rb
+++ lib/commendo/content_set.rb
@@ -10,10 +10,56 @@ module Commendo
 
     def add(group, *resources)
       resources.each do |res|
-        redis.sadd("#{key_base}:#{res}", group)
+        redis.sadd("#{set_key_base}:#{res}", group)
       end
     end
 
+    def calculate_similarity
+      outer_cursor = '0'
+      begin
+        outer_cursor, outer_keyources = redis.scan(outer_cursor, match: "#{set_key_base}:*")
+        outer_keyources.each do |outer_key|
+
+          inner_cursor = '0'
+          begin
+            inner_cursor, inner_keyources = redis.scan(inner_cursor, match: "#{set_key_base}:*")
+            similar = []
+            inner_keyources.each do |inner_key|
+              next if inner_key == outer_key
+              intersect = redis.sinter(outer_key, inner_key).length
+              if intersect > 0
+                union = redis.sunion(outer_key, inner_key).length
+                similarity = intersect / union.to_f
+                similar << inner_key.gsub(/^#{set_key_base}:/, '')
+                similar << similarity
+              end
+              outer_res = outer_key.gsub(/^#{set_key_base}:/, '')
+              redis.hmset("#{similar_key_base}:#{outer_res}", similar) unless similar.empty?
+            end
+          end while (inner_cursor != '0')
+
+        end
+      end while (outer_cursor != '0')
+    end
+
+    def similar_to(resource)
+      similar = []
+      similar_resources = redis.hgetall("#{similar_key_base}:#{resource}")
+      similar_resources.each do |resource, similarity|
+        similar << {resource: resource, similarity: similarity.to_f}
+      end
+      similar.sort! { |x,y| y[:similarity] <=> x[:similarity] }
+    end
+
+    private
+
+    def set_key_base
+      "#{key_base}:sets"
+    end
+
+    def similar_key_base
+      "#{key_base}:similar"
+    end
 
   end
 
--- test/content_set_test.rb
+++ test/content_set_test.rb
@@ -16,15 +16,37 @@ module Commendo
       cs = ContentSet.new(redis, key_base)
       cs.add('group-1', 'resource-1', 'resource-2', 'resource-3')
       cs.add('group-2', 'resource-1', 'resource-3', 'resource-4')
-      assert redis.sismember("#{key_base}:resource-1", 'group-1')
-      assert redis.sismember("#{key_base}:resource-2", 'group-1')
-      assert redis.sismember("#{key_base}:resource-3", 'group-1')
-      refute redis.sismember("#{key_base}:resource-4", 'group-1')
+      assert redis.sismember("#{key_base}:sets:resource-1", 'group-1')
+      assert redis.sismember("#{key_base}:sets:resource-2", 'group-1')
+      assert redis.sismember("#{key_base}:sets:resource-3", 'group-1')
+      refute redis.sismember("#{key_base}:sets:resource-4", 'group-1')
 
-      assert redis.sismember("#{key_base}:resource-1", 'group-2')
-      refute redis.sismember("#{key_base}:resource-2", 'group-2')
-      assert redis.sismember("#{key_base}:resource-3", 'group-2')
-      assert redis.sismember("#{key_base}:resource-4", 'group-2')
+      assert redis.sismember("#{key_base}:sets:resource-1", 'group-2')
+      refute redis.sismember("#{key_base}:sets:resource-2", 'group-2')
+      assert redis.sismember("#{key_base}:sets:resource-3", 'group-2')
+      assert redis.sismember("#{key_base}:sets:resource-4", 'group-2')
+    end
+
+    def test_calculates_similarity_scores
+      redis = Redis.new(db: 15)
+      redis.flushdb
+      key_base = 'CommendoTests'
+      cs = ContentSet.new(redis, key_base)
+      (3..23).each do |group|
+        (3..23).each do |res|
+          cs.add(group, res) if res % group == 0
+        end
+      end
+      cs.calculate_similarity
+      expected = [
+        { resource:  '9', similarity: 0.5 },
+        { resource:  '6', similarity: 0.5 },
+        { resource: '12', similarity: 0.3333333333333333 },
+        { resource:  '3', similarity: 0.25 },
+        { resource: '21', similarity: 0.16666666666666666 },
+        { resource: '15', similarity: 0.16666666666666666 }
+      ]
+      assert_equal expected, cs.similar_to(18)
     end
 
   end
