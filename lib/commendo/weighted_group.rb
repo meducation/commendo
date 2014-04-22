b252e92652645edddbaa47955700de5cfc043c68
Rob Styles <rob.styles@dynamicorange.com>
2014-04-22 12:20:33 +0100

    Limiting numbers on weighted groups

--- lib/commendo/weighted_group.rb
+++ lib/commendo/weighted_group.rb
@@ -8,7 +8,8 @@ module Commendo
       @content_sets, @redis, @key_base = content_sets, redis, key_base
     end
 
-    def similar_to(resource)
+    def similar_to(resource, limit = 0)
+      finish = limit -1
       resources = resource.kind_of?(Array) ? resource : [resource]
       keys = []
       weights = []
@@ -20,7 +21,7 @@ module Commendo
       end
       tmp_key = "#{key_base}:tmp:#{SecureRandom.uuid}"
       redis.zunionstore(tmp_key, keys, weights: weights)
-      similar_resources = redis.zrevrange(tmp_key, 0, -1, with_scores: true)
+      similar_resources = redis.zrevrange(tmp_key, 0, finish, with_scores: true)
       redis.del(tmp_key)
 
       similar_resources.map do |resource|
@@ -30,11 +31,18 @@ module Commendo
     end
 
     def filtered_similar_to(resource, options = {})
-      similar = similar_to(resource)
-      return similar if @tag_set.nil? || options[:include].nil? && options[:exclude].nil?
-      similar.delete_if { |s| !options[:exclude].nil? && @tag_set.matches(s[:resource], options[:exclude]) }
-      similar.delete_if { |s| !options[:include].nil? && !@tag_set.matches(s[:resource], options[:include]) }
-      similar
+      if @tag_set.nil? || (options[:include].nil? && options[:exclude].nil?)
+        return similar_to(resource, options[:limit] || 0)
+      else
+        similar = similar_to(resource)
+        limit = options[:limit] || similar.length
+        filtered = []
+        similar.each do |s|
+          return filtered if filtered.length >= limit
+          filtered << s if @tag_set.matches(s[:resource], options[:include], options[:exclude])
+        end
+        return filtered
+      end
     end
 
   end
